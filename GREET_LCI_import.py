7d6efad32b526e5b4df0548f2294f0f0861e6e63
saurajyoti.kar@gmail.com <JustGi56>
2023-02-23 16:08:31 -0600

    modified code to accept multiple global arguments

--- GREET_LCI_import.py
+++ GREET_LCI_import.py
@@ -45,18 +45,19 @@ class GREET_LCI_import:
                                         self.sheet_input, header=3, index_col=None)
         
     
-    def modify_model_and_run(self, gparam_val, df_params):
+    def modify_model_and_run(self, gparam_index, gparam_val, df_params):
         
         with xw.App(visible=False) as app:            
             
             # open model workbook
             wb = xw.Book(model_path_prefix + '/' + fmodel)
             
-            # modify model with global parameter
-            sheet = wb.sheets[self.sheet_gparam]
-            sheet[cell_gparam].value = gparam_val
-            wb.app.calculate()
-            wb.save()
+            # modify model with global parameters
+            for idx, val in enumerate(gparam_val):                
+                sheet = wb.sheets[self.sheet_gparam[idx]]
+                sheet[self.cell_gparam[idx]].value = val
+                wb.app.calculate()
+                wb.save()
             
             # modify model with set of parameters            
             for r in range(df_params.shape[0]):
@@ -86,17 +87,17 @@ class GREET_LCI_import:
         self.save_sim_to_file(mode='w', header=False)
         write_header = True
         
-        for gparam_val in gparam:
+        for gparam_index, gparam_val in enumerate(gparam):
             
             for param_set in range(0, n_param_sets):
                 
                 df_params = self.sim_params.iloc[:,[1,2,param_set+3]]
                 
-                print(f'Run for global parameter value: {gparam_val} and parameter set: {param_set+1}') 
+                print(f'Executing global parameter set: {gparam_val} and parameter set {param_set+1} out of {n_param_sets}') 
                 print( '    Elapsed time: ' + str(datetime.now() - init_time))
                 
-                self.modify_model_and_run(gparam_val, df_params)                
-                self.temp_corr_LCI['gparam_val'] = gparam_val
+                self.modify_model_and_run(gparam_index, gparam_val, df_params)                
+                self.temp_corr_LCI['gparam_val'] = '-'.join(map(str,gparam_val))
                 self.temp_corr_LCI['param_set'] = param_set + 1
                 
                 #self.sim_df = pd.concat([self.sim_df, self.temp_corr_LCI.copy()], ignore_index=True)
@@ -114,20 +115,28 @@ if __name__ == '__main__':
     init_time = datetime.now()
     
     model_path_prefix = 'C:/Users/skar/Box/saura_self/Proj - Algae/data/model'
-    fmodel = 'GREET_2022 Algae harmonization project_HTL_paper.xlsm'
+    fmodel = 'GREET_2022 Algae harmonization project_HTL_paper_1.xlsm'
     
     corr_path_prefix = 'C:/Users/skar/Box/saura_self/Proj - Algae/data/correspondence_files'
-    fcorr_LCI = 'corr_LCI_GREET_pathway_Algae_run.xlsx'
-    sheet_input = 'param_inputs'
+    fcorr_LCI = 'corr_LCI_GREET_pathway_Algae_v1_run.xlsx'
+    sheet_input = 'PC'
     sheet_output = 'map_outputs'
     
-    fsave_sim = 'GREET_Algae_sims.csv'
+    fsave_sim = 'GREET_Algae_sims_PC.csv'
     
-    
-    # list of global parameter to run all scenarios
-    gparam = [1]    
-    sheet_gparam = 'Algae' # the sheet in fmodel that has the parameter
-    cell_gparam = 'AI556' # the cell in fmodel sheet_gparam where parameter is located
+    # Global parameter declarations
+    sheet_gparam = ['Algae', 'Algae'] # the sheets in fmodel that has the parameters
+    cell_gparam = ['AI556', 'AF555'] # the cells in fmodel sheet_gparam where parameters are located
+    gparam = [[1,1],  # value set of global parameters
+              #[2,1],
+              #[3,1],
+              #[1,2],
+              #[2,2],
+              #[3,2],
+              #[1,3],
+              #[2,3],
+              #[3,3]
+              ]        
     
     obj = GREET_LCI_import(model_path_prefix, fmodel,
                            corr_path_prefix, fcorr_LCI, sheet_input, sheet_output,
