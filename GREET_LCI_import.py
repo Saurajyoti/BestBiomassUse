86e99a95eac204faebcb36afd0a7b222f0a91af7
Kar <skar@anl.gov>
2023-03-02 09:22:00 -0600

    performance inprovement, open workbook once

--- GREET_LCI_import.py
+++ GREET_LCI_import.py
@@ -11,16 +11,27 @@ Collect LCI data by year from GREET1 as per correspondence file.
 #%%
 
 import pandas as pd
+import numpy as np
 import xlwings as xw
 from datetime import datetime
 
+# %% Customize the Excel Instance
+
+class ExcelApp(xw.App):
+    """override xw.App default properties"""
+    calculation = 'manual'
+    display_alerts = False
+    enable_events = False
+    screen_updating = False
+    visible = False
+    
 #%%
 # GREET1 model run for study years
 
 class GREET_LCI_import:
     
     def __init__(self, model_path_prefix, fmodel,
-                 corr_path_prefix, fcorr_LCI, sheet_input, sheet_output,
+                 corr_path_prefix, fcorr_LCI, sheet_input, 
                  gparam, sheet_gparam, cell_gparam,
                  fsave_sim):
         
@@ -30,7 +41,6 @@ class GREET_LCI_import:
         self.corr_path_prefix = corr_path_prefix
         self.fcorr_LCI = fcorr_LCI
         self.sheet_input = sheet_input
-        self.sheet_output = sheet_output
         
         self.gparam = gparam        
         self.sheet_gparam = sheet_gparam
@@ -45,35 +55,6 @@ class GREET_LCI_import:
                                         self.sheet_input, header=3, index_col=None)
         
     
-    def modify_model_and_run(self, gparam_index, gparam_val, df_params):
-        
-        with xw.App(visible=False) as app:            
-            
-            # open model workbook
-            wb = xw.Book(model_path_prefix + '/' + fmodel)
-            
-            # modify model with global parameters
-            for idx, val in enumerate(gparam_val):                
-                sheet = wb.sheets[self.sheet_gparam[idx]]
-                sheet[self.cell_gparam[idx]].value = val
-                wb.app.calculate()
-                wb.save()
-            
-            # modify model with set of parameters            
-            for r in range(df_params.shape[0]):
-                sheet = wb.sheets[df_params.iloc[r,0]]
-                sheet[df_params.iloc[r,1]].value = df_params.iloc[r,2]
-            wb.app.calculate()
-            wb.save()
-            
-            # update output sheet
-            wb2 = xw.Book(self.corr_path_prefix + '/' + self.fcorr_LCI)
-            wb2.app.calculate()
-            wb2.save()
-            
-        self.temp_corr_LCI = pd.read_excel(self.corr_path_prefix + '/' + self.fcorr_LCI, 
-                                           self.sheet_output, header=3, index_col=None)
-    
     def save_sim_to_file(self, mode, header):
         
         self.sim_df.to_csv(self.corr_path_prefix + '/' + self.fsave_sim, 
@@ -84,30 +65,51 @@ class GREET_LCI_import:
         n_param_sets = self.sim_params.shape[1] - 3
         
         # truncate if file exists and create
-        self.save_sim_to_file(mode='w', header=False)
+        #self.save_sim_to_file(mode='w', header=False)
         write_header = True
         
-        for gparam_index, gparam_val in enumerate(gparam):
+        
+        with ExcelApp() as app:            
             
-            for param_set in range(0, n_param_sets):
+            # open model workbook
+            wb = xw.Book(model_path_prefix + '/' + fmodel)
+        
+            for gparam_index, gparam_val in enumerate(gparam):
                 
-                df_params = self.sim_params.iloc[:,[1,2,param_set+3]]
-                
-                print(f'Executing global parameter set: {gparam_val} and parameter set {param_set+1} out of {n_param_sets}') 
-                print( '    Elapsed time: ' + str(datetime.now() - init_time))
-                
-                self.modify_model_and_run(gparam_index, gparam_val, df_params)                
-                self.temp_corr_LCI['gparam_val'] = '-'.join(map(str,gparam_val))
-                self.temp_corr_LCI['param_set'] = param_set + 1
-                
-                #self.sim_df = pd.concat([self.sim_df, self.temp_corr_LCI.copy()], ignore_index=True)
-                self.sim_df = self.temp_corr_LCI.copy()
-                
-                if write_header:
-                    self.save_sim_to_file(mode='a', header=write_header) # append output to file
-                    write_header = False
-                else:
-                    self.save_sim_to_file(mode='a', header=write_header) # append output to file
+                for param_set in range(0, n_param_sets):
+                    
+                    df_params = self.sim_params.iloc[:,[1,2,param_set+3]]
+                    
+                    print(f'Executing global parameter set: {gparam_val} and parameter set {param_set+1} out of {n_param_sets}') 
+                    print( '    Elapsed time: ' + str(datetime.now() - init_time))
+                    
+                    # modify model with global parameters
+                    for idx, val in enumerate(gparam_val):                
+                        sheet = wb.sheets[self.sheet_gparam[idx]]
+                        sheet[self.cell_gparam[idx]].value = val                
+                    
+                    # modify model with set of parameters            
+                    for r in range(df_params.shape[0]):
+                        if ~np.isnan(df_params.iloc[r,2]):
+                            sheet = wb.sheets[df_params.iloc[r,0]]
+                            sheet[df_params.iloc[r,1]].value = df_params.iloc[r,2]
+                    
+                    wb.app.calculate()
+                               
+                    # Extract data
+                    sheet = wb.sheets['Algae_results']
+                    self.sim_df = sheet['A1:E25'].options(pd.DataFrame).value 
+                    
+                    self.sim_df['gparam_val'] = '-'.join(map(str,gparam_val))
+                    self.sim_df['param_set'] = param_set + 1                              
+                                
+                    check_time = datetime.now()
+                    if write_header:
+                        self.save_sim_to_file(mode='a', header=write_header) # append output to file
+                        write_header = False
+                    else:
+                        self.save_sim_to_file(mode='a', header=write_header) # append output to file
+                    print(f'File save time {str(datetime.now() - check_time)}')
         
 
 if __name__ == '__main__':
@@ -119,27 +121,26 @@ if __name__ == '__main__':
     
     corr_path_prefix = 'C:/Users/skar/Box/saura_self/Proj - Algae/data/correspondence_files'
     fcorr_LCI = 'corr_LCI_GREET_pathway_Algae_v1_run.xlsx'
-    sheet_input = 'PC'
-    sheet_output = 'map_outputs'
+    sheet_input = 'PC'   
     
-    fsave_sim = 'GREET_Algae_sims_PC.csv'
+    fsave_sim = 'GREET_Algae_sims_'+sheet_input+'.csv'
     
     # Global parameter declarations
     sheet_gparam = ['Algae', 'Algae'] # the sheets in fmodel that has the parameters
     cell_gparam = ['AI556', 'AF555'] # the cells in fmodel sheet_gparam where parameters are located
-    gparam = [[1,1],  # value set of global parameters
+    gparam = [#[1,1],  # value set of global parameters
               #[2,1],
               #[3,1],
               #[1,2],
               #[2,2],
-              #[3,2],
-              #[1,3],
-              #[2,3],
-              #[3,3]
+              [3,2],
+              [1,3],
+              [2,3],
+              [3,3]
               ]        
     
     obj = GREET_LCI_import(model_path_prefix, fmodel,
-                           corr_path_prefix, fcorr_LCI, sheet_input, sheet_output,
+                           corr_path_prefix, fcorr_LCI, sheet_input,
                            gparam, sheet_gparam, cell_gparam,
                            fsave_sim)
     
